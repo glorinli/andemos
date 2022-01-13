52cf99e13d126e27fa2b801bed62dc674336d2f3
glorinli <ligr1991@gmail.com>
2022-01-13 14:42:26 +0800

    Add fresco demo

---
+++ FrescoDemo/app/src/main/java/xyz/glorin/frescodemo/App.kt
@@ -0,0 +1,12 @@
+package xyz.glorin.frescodemo
+
+import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Fresco.initialize(this)
+    }
+}
--- FrescoDemo/app/src/main/java/xyz/glorin/frescodemo/MainActivity.kt
+++ FrescoDemo/app/src/main/java/xyz/glorin/frescodemo/MainActivity.kt
@@ -1,11 +1,18 @@
 package xyz.glorin.frescodemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.drawee.view.SimpleDraweeView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<SimpleDraweeView>(R.id.draweeView).setImageURI(IMAGE_URL)
+    }
+
+    companion object {
+        private const val IMAGE_URL = "https://frescolib.org/static/logo.png"
     }
 }
